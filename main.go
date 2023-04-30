eae37575019ef5052f1dd9ee46e79220e60a8c35
alexis roberson <akroberson18@gmail.com>
2023-04-30 12:01:42 -0500

    Updated http router to handle traffic

--- main.go
+++ main.go
@@ -3,13 +3,27 @@ package main
 import (
         "fmt"
         "net/http"
+        "github.com/julienschmidt/httprouter"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+        w.Header().Set("Content-Type", "text/html")
         fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+        w.Header().Set("Content-Type", "text/html")
+        fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@gallerywalk.com\">support@gallerywalk.com</a>.")
+}
+func notFound(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/html")
+        w.WriteHeader(http.StatusNotFound)
+        fmt.Fprint(w, "<h1>Sorry, but we couldn't find the page you were looking for</h1>")
+}
 func main() {
-        http.HandleFunc("/", handlerFunc)
-        http.ListenAndServe(":3000", nil)
+        router := httprouter.New()
+        router.GET("/", home)
+        router.GET("/contact", contact)
+        router.NotFound = http.HandlerFunc(notFound)
+        http.ListenAndServe(":3000", router)
 }
